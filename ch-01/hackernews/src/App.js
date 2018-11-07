03b62d2e1a8481af1f2a1246e1a9b78dc7e3c89a
mtraina <matteo.traina.mail@gmail.com>
2018-11-07 19:45:38 +0100

    hackernews playing with list

--- ch-01/hackernews/src/App.js
+++ ch-01/hackernews/src/App.js
@@ -1,12 +1,30 @@
 import React, { Component } from 'react';
 import './App.css';
 
+const list = [
+  {
+    title: 'React',
+    url: 'https://reactjs.org/',
+    author: 'Jordan Walke',
+    num_comments: 3,
+    points: 4,
+    objectID: 0,
+  }, {
+    title: 'Redux',
+    url: 'https://redux.js.org/',
+    author: 'Dan Abramov, Andrew Clark',
+    num_comments: 2,
+    points: 5,
+    objectID: 1,
+  },];
+
 class App extends Component {
   render() {
-    var helloWorld = 'Welcome to the Road to learn React';
     return (
       <div className="App">
-        <h2>{helloWorld}</h2>
+        {list.map(function (item) {
+          return <div>{item.title}</div>;
+        })}
       </div>);
   }
 }
