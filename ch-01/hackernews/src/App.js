8e423cfa6190bac900932a4126bdc4e505ed00eb
mtraina <matteo.traina.mail@gmail.com>
2018-11-07 19:49:38 +0100

    hackernews playing with list 2

--- ch-01/hackernews/src/App.js
+++ ch-01/hackernews/src/App.js
@@ -23,7 +23,15 @@ class App extends Component {
     return (
       <div className="App">
         {list.map(function (item) {
-          return <div>{item.title}</div>;
+          return (
+            <div key={item.objectID}>
+              <span>
+                <a href={item.url}>{item.title}</a>
+              </span>
+              <span>{item.author}</span>
+              <span>{item.num_comments}</span>
+              <span>{item.points}</span>
+            </div>);
         })}
       </div>);
   }
