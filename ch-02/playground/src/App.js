461e6cb7d74557e4ed215ebf31b797c066741818
mtraina <matteo.traina.mail@gmail.com>
2018-11-07 21:29:59 +0100

    chapter two playground css 2

--- ch-02/playground/src/App.js
+++ ch-02/playground/src/App.js
@@ -73,25 +73,24 @@ const Table = ({ list, pattern, onDismiss }) =>
   <div className="table">
     {list.filter(isSearched(pattern)).map(item =>
       <div key={item.objectID} className="table-row">
-        <span>
+        <span style={{ width: '40%' }}>
           <a href={item.url}>{item.title}</a>
         </span>
-        <span>{item.author}</span>
-        <span>{item.num_comments}</span>
-        <span>{item.points}</span>
-        <span>
-          <Button onClick={() => onDismiss(item.objectID)} className="button-inline" >
+        <span style={{ width: '30%' }}>{item.author}</span>
+        <span style={{ width: '10%' }}>{item.num_comments}</span>
+        <span style={{ width: '10%' }}>{item.points}</span>
+        <span style={{ width: '10%' }}>
+          <Button onClick={() => onDismiss(item.objectID)} className="button-inline" label="Dismiss">
             Dismiss
           </Button>
         </span>
       </div>)}
   </div>
 
-const Button = ({ onClick, className = '', children }) =>
+const Button = ({ onClick, className = '', children, label }) =>
   <button
     onClick={onClick}
-    className={className}
-  type="button">
+    className={className}>{label}
   </button>
 
 export default App
