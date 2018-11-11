dd64945cd8266cfa25f00ef20928730de39c939f
mtraina <matteo.traina.mail@gmail.com>
2018-11-11 05:10:51 +0100

    chapter 5 - pagintion I

--- ch-05/hackernews/src/App.js
+++ ch-05/hackernews/src/App.js
@@ -3,9 +3,13 @@ import logo from './logo.svg';
 import './App.css';
 
 const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
+
 const PATH_BASE = 'https://hn.algolia.com/api/v1';
 const PATH_SEARCH = '/search';
 const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
 
 class App extends Component {
   constructor(props) {
@@ -24,11 +28,21 @@ class App extends Component {
   }
 
   setSearchTopStories(result) {
-    this.setState({ result });
+    const { hits, page } = result;
+    const oldHits = page !== 0
+      ? this.state.result.hits
+      : [];
+    const updatedHits = [
+      ...oldHits,
+      ...hits
+    ];
+    this.setState({
+      result: { hits: updatedHits, page }
+    });
   }
 
-  fetchSearchTopStories(searchTerm) {
-    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}`)
+  fetchSearchTopStories(searchTerm, page = 0) {
+    fetch(`${PATH_BASE}${PATH_SEARCH}?${PARAM_SEARCH}${searchTerm}&${PARAM_PAGE}${page}`)
       .then(response => response.json())
       .then(result => this.setSearchTopStories(result))
       .catch(error => error);
@@ -56,7 +70,7 @@ class App extends Component {
 
   render() {
     const { searchTerm, result } = this.state;
-
+    const page = (result && result.page) || 0;
 
     return (
       <div className="page">
@@ -73,6 +87,11 @@ class App extends Component {
             list={result.hits}
             onDismiss={this.onDismiss}
           />}
+        <div className="interactions">
+          <Button onClick={() => this.fetchSearchTopStories(searchTerm, page + 1)}>
+            More
+          </Button>
+        </div>
       </div>);
   }
 
@@ -90,7 +109,7 @@ const Search = ({ value, onChange, onSubmit, children }) =>
     <input
       type="text"
       value={value}
-      onChange={onChange}/>
+      onChange={onChange} />
     <button type="submit">
       {children}
     </button>
