5f03674650c51d2d487a8a5984551a7322b9f554
mtraina <matteo.traina.mail@gmail.com>
2018-11-12 15:22:25 -1000

    chapter 6 - testing III

--- ch-06/hackernews/src/components/App/test.js
+++ ch-06/hackernews/src/components/App/test.js
@@ -1,7 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import renderer from 'react-test-renderer';
-import App from '../App';
+import App, { Search } from '../App';
 
 describe('App', () => {
 
@@ -18,4 +18,19 @@ describe('App', () => {
     expect(tree).toMatchSnapshot();
   });
 
+});
+
+describe('Search', () => {
+  it('renders without crashing', () => {
+    const div = document.createElement('div');
+    ReactDOM.render(<Search>Search</Search>, div);
+    ReactDOM.unmountComponentAtNode(div);
+  });
+  test('has a valid snapshot', () => {
+    const component = renderer.create(
+      <Search>Search</Search>
+    );
+    const tree = component.toJSON();
+    expect(tree).toMatchSnapshot();
+  });
 });
