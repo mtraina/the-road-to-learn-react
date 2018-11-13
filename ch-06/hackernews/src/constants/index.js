1049e6e8d84e9545df92fde0c156f02dbdea84cd
mtraina <matteo.traina.mail@gmail.com>
2018-11-12 14:29:23 -1000

    chapter 6 - modularity I

---
+++ ch-06/hackernews/src/constants/index.js
@@ -0,0 +1,7 @@
+const DEFAULT_QUERY = 'redux';
+const DEFAULT_HPP = '100';
+const PATH_BASE = 'https://hn.algolia.com/api/v1';
+const PATH_SEARCH = '/search';
+const PARAM_SEARCH = 'query=';
+const PARAM_PAGE = 'page=';
+const PARAM_HPP = 'hitsPerPage=';
