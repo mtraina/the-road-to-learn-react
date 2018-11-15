10a2768fac2c6920ea1b97b129b7552491e4101d
mtraina <matteo.traina.mail@gmail.com>
2018-11-14 20:38:48 -1000

    chapter 7 mods

---
+++ ch-07/hackernews/src/components/Table/index.js
@@ -0,0 +1,29 @@
+import React from 'react';
+import Button from '../Button';
+import './index.css';
+import PropTypes from 'prop-types';
+
+const Table = ({ list, onDismiss }) =>
+    <div className="table">
+        {list.map(item =>
+            <div key={item.objectID} className="table-row">
+                <span style={{ width: '40%' }}>
+                    <a href={item.url}>{item.title}</a>
+                </span>
+                <span style={{ width: '30%' }}>{item.author}</span>
+                <span style={{ width: '10%' }}>{item.num_comments}</span>
+                <span style={{ width: '10%' }}>{item.points}</span>
+                <span style={{ width: '10%' }}>
+                    <Button onClick={() => onDismiss(item.objectID)} className="button-inline" label="Dismiss">
+                        Dismiss
+                    </Button>
+                </span>
+            </div>)}
+    </div>
+
+Table.propTypes = {
+    list: PropTypes.array.isRequired,
+    onDismiss: PropTypes.func.isRequired,
+};
+
+export default Table;
